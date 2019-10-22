dc5ce51ca7a7c139935f14643f3c80346e19e498
Arman Mazdaee <arman.maz1373@gmail.com>
2019-10-22 15:33:51 +0330

    add numpy and mido as dependency

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     entry_points={"console_scripts": ["perfectpitch = perfectpitch:main"]},
     python_requires=">=3.6,<4.0",
     install_requires=[
+        "numpy>=1.17.0,<2.0.0",
+        "mido>=1.2.0,<2.0.0",
         "tensorflow-gpu>=2.0.0,<3.0.0" if GPU else "tensorflow>=2.0.0,<3.0.0",
         "librosa>=0.7.0,<0.8.0",
         "mir_eval>=0.5.0,<0.6.0",
