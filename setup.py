0d2ff7722bb51490ee8dfb91d402ddfcc60b94f3
Arman Mazdaee <arman.maz1373@gmail.com>
2019-12-06 11:20:23 +0330

    add pytorch-lightning

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ setup(
     entry_points={"console_scripts": ["perfectpitch = perfectpitch:main"]},
     python_requires=">=3.6,<4.0",
     install_requires=[
-        "numpy>=1.17.0,<2.0.0",
+        "torch>=1.3.0,<2.0.0",
+        "pytorch-lightning>=0.5.3,<0.6.0",
+        "numpy==1.16.4",
         "h5py>=2.8.0,<3.0.0",
         "mido>=1.2.0,<2.0.0",
-        "torch>=1.3.0,<2.0.0",
         "librosa>=0.7.0,<0.8.0",
         "mir_eval>=0.5.0,<0.6.0",
-        "tqdm>=4.28.0,<5.0.0",
     ],
     extras_require={"prepare_dataset": ["magenta>=1.1.0,<2.0.0"]},
 )
