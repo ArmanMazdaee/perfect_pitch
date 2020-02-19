4bee84045657ad44f087e32b720e7d07de6605db
Arman Mazdaee <arman.maz1373@gmail.com>
2020-02-19 15:53:16 +0330

    change the cmd structer

--- perfectpitch/__init__.py
+++ perfectpitch/__init__.py
@@ -1,8 +1,70 @@
-from perfectpitch import cmd
+import argparse
 
 
 def main():
-    cmd.run()
+    parser = argparse.ArgumentParser(
+        description="Automatic Music Transcription using deeplearning"
+    )
+    subparsers = parser.add_subparsers(title="command")
+    subparsers.required = True
+
+    acoustic_dataset_parser = subparsers.add_parser(
+        "acoustic-dataset",
+        help="Prepare the MAPS dataset for the acoustic model",
+        description=(
+            "Preparing the MAPS datasets for training and validating the acoustic model"
+        ),
+    )
+    acoustic_dataset_parser.set_defaults(
+        func=lambda input_path, output_path: print(
+            "acoustic-dataset", input_path, output_path
+        )
+    )
+    acoustic_dataset_parser.add_argument(
+        "--input",
+        "-i",
+        required=True,
+        help="Path of the MAPS dataset",
+        dest="input_path",
+    )
+    acoustic_dataset_parser.add_argument(
+        "--output",
+        "-o",
+        required=True,
+        help="Path of the prepared dataset",
+        dest="output_path",
+    )
+
+    acoustic_train_parser = subparsers.add_parser(
+        "acoustic-train",
+        help="Train the acoustic model",
+        description="Train the acoustic model",
+    )
+    acoustic_train_parser.set_defaults(
+        func=lambda dataset_path, use_gpu: print(
+            "acoustic-trian", dataset_path, use_gpu
+        )
+    )
+    acoustic_train_parser.add_argument(
+        "--dataset-path",
+        "-d",
+        required=True,
+        help="Path of the dataset",
+        dest="dataset_path",
+    )
+    acoustic_train_parser.add_argument(
+        "--gpu",
+        "-g",
+        action="store_true",
+        help="Use gpu for training the model",
+        dest="use_gpu",
+    )
+
+    args = parser.parse_args()
+    func = args.func
+    kwargs = vars(args)
+    del kwargs["func"]
+    func(**kwargs)
 
 
 if __name__ == "__main__":
