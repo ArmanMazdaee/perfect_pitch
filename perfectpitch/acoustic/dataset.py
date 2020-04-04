a385ad24645477d341a78942cc3ca123de01aeae
Arman Mazdaee <arman.maz1373@gmail.com>
2020-04-04 18:33:40 +0430

    add base of acoustic load_dataset

--- perfectpitch/acoustic/dataset.py
+++ perfectpitch/acoustic/dataset.py
@@ -1,27 +1,94 @@
-import os
-from glob import glob
+import tensorflow as tf
 
-import numpy as np
-import torch
+from perfectpitch import constants
+from perfectpitch.dataset.load import load_dataset as load_transcription_dataset
 
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, path, split):
-        sample_glob = os.path.join(path, split, "*.npz")
-        self.__sample_filenames = sorted(glob(sample_glob))
-        if len(self.__sample_filenames) == 0:
-            raise ValueError("Wrong dataset path or split")
+def _create_pianoroll__create_actives_indicies(pitches, onsets, offsets):
+    length = tf.shape(pitches)[0]
+    _, frame_indicies, pitch_indicies = tf.while_loop(
+        cond=lambda i, frame_indicies, pitch_indicies: i < length,
+        body=lambda i, frame_indicies, pitch_indicies: (
+            i + 1,
+            tf.concat([frame_indicies, tf.range(onsets[i], offsets[i])], axis=0),
+            tf.concat(
+                [pitch_indicies, tf.fill([offsets[i] - onsets[i]], pitches[i])], axis=0
+            ),
+        ),
+        loop_vars=(
+            tf.constant(0, tf.dtypes.int32),
+            tf.zeros([0], tf.dtypes.int32),
+            tf.zeros([0], tf.dtypes.int32),
+        ),
+        shape_invariants=(
+            tf.TensorShape([]),
+            tf.TensorShape([None]),
+            tf.TensorShape([None]),
+        ),
+        parallel_iterations=1,
+    )
+    return tf.stack([frame_indicies, pitch_indicies], axis=1)
 
-    def __len__(self):
-        return len(self.__sample_filenames)
 
-    def __getitem__(self, index):
-        sample = np.load(self.__sample_filenames[index])
-        spec = torch.from_numpy(sample["spec"])
-        pianoroll = {
-            "actives": torch.from_numpy(sample["pianoroll_actives"]),
-            "onsets": torch.from_numpy(sample["pianoroll_onsets"]),
-            "offsets": torch.from_numpy(sample["pianoroll_offsets"]),
-            "velocities": torch.from_numpy(sample["pianoroll_velocities"]),
-        }
-        return spec, pianoroll
+def _create_pianoroll__create_frame(shape, indices):
+    num_values = tf.shape(indices)[0]
+    values = tf.fill([num_values], tf.constant(1, tf.dtypes.float32))
+    return tf.scatter_nd(indices, values, shape)
+
+
+@tf.function
+def _create_pianoroll(notesequence, num_frames):
+    frame_duration = constants.SPEC_HOP_LENGTH / constants.SAMPLE_RATE
+    num_pitches = constants.MAX_PITCH - constants.MIN_PITCH + 1
+    frame_shape = [num_frames, num_pitches]
+
+    pitches = tf.cast(notesequence["pitches"] - constants.MIN_PITCH, tf.dtypes.int32)
+    onsets = tf.cast(notesequence["intervals"][:, 0] / frame_duration, tf.dtypes.int32)
+    onsets = tf.minimum(onsets, num_frames - 1)
+    offsets = tf.cast(notesequence["intervals"][:, 1] / frame_duration, tf.dtypes.int32)
+    offsets = tf.minimum(offsets, num_frames - 1)
+    velocities = tf.cast(
+        notesequence["velocities"] / tf.reduce_max(notesequence["velocities"]),
+        tf.dtypes.float32,
+    )
+
+    valid_indices = pitches >= 0
+    valid_indices = tf.logical_and(valid_indices, pitches < num_pitches)
+    valid_indices = tf.logical_and(valid_indices, tf.not_equal(onsets, offsets))
+    pitches = pitches[valid_indices]
+    onsets = onsets[valid_indices]
+    offsets = offsets[valid_indices]
+    velocities = velocities[valid_indices]
+
+    actives_indicies = _create_pianoroll__create_actives_indicies(
+        pitches, onsets, offsets
+    )
+    actives_frame = _create_pianoroll__create_frame(frame_shape, actives_indicies)
+
+    onsets_indices = tf.stack([onsets, pitches], axis=1)
+    onsets_frame = _create_pianoroll__create_frame(frame_shape, onsets_indices)
+    velocities_frame = tf.scatter_nd(onsets_indices, velocities, frame_shape)
+
+    offsets_indices = tf.stack([offsets, pitches], axis=1)
+    offsets_frame = _create_pianoroll__create_frame(frame_shape, offsets_indices)
+
+    return {
+        "actives": tf.clip_by_value(actives_frame, 0, 1),
+        "onsets": tf.clip_by_value(onsets_frame, 0, 1),
+        "offsets": tf.clip_by_value(offsets_frame, 0, 1),
+        "velocities": tf.clip_by_value(velocities_frame, 0, 1),
+    }
+
+
+def _preprocess_sample(sample):
+    spec = sample["spec"]
+    num_frames = tf.shape(spec)[0]
+    pianoroll = _create_pianoroll(sample["notesequence"], num_frames)
+    return spec, pianoroll
+
+
+def load_dataset(path, split):
+    ordered = False if split == "train" else True
+    dataset = load_transcription_dataset(path, split, ordered)
+    dataset = dataset.map(_preprocess_sample, tf.data.experimental.AUTOTUNE)
+    return dataset
