5f15b2047b993e547fb5282556181448e3da543f
Arman Mazdaee <arman.maz1373@gmail.com>
2020-04-14 16:33:52 +0430

    add convert-maestro command

---
+++ perfectpitch/dataset/maestro.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+from .create_transcription_dataset import create_transcription_dataset
+
+
+def convert_maestro(input_path, output_path):
+    info_filename = os.path.join(input_path, "maestro-v1.0.0.json")
+    with open(info_filename) as file:
+        info = json.load(file)
+
+    for split in ["train", "validation", "test"]:
+        names = [
+            sample["audio_filename"][5:-4]
+            for sample in info
+            if sample["split"] == split
+        ]
+        wav_filenames = [
+            os.path.join(input_path, sample["audio_filename"])
+            for sample in info
+            if sample["split"] == split
+        ]
+        midi_filenames = [
+            os.path.join(input_path, sample["midi_filename"])
+            for sample in info
+            if sample["split"] == split
+        ]
+        create_transcription_dataset(
+            os.path.join(output_path, split), names, wav_filenames, midi_filenames,
+        )
