894a471e1ea1faa28aa2fca1fab19509fed7021b
Arman Mazdaee <arman.maz1373@gmail.com>
2020-04-18 20:17:43 +0430

    add acoustic model _Conv2dStack

--- perfectpitch/acoustic/model.py
+++ perfectpitch/acoustic/model.py
@@ -1,76 +1,55 @@
-import math
-
 import torch
 
 from perfectpitch import constants
 
 
-def _binary_cross_entropy_with_logits(input, target, weight):
-    loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        input, target, reduction="none"
-    )
-    return (loss * weight).sum() / weight.sum()
-
-
 class _Conv2dStack(torch.nn.Sequential):
-    def __init__(self, in_channels, out_channels, dropout):
-        super().__init__()
-        self.conv2ds = torch.nn.Sequential(
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=1, out_channels=48, kernel_size=3, padding=1,
-                )
-            ),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(dropout),
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=48,
-                    out_channels=48,
-                    kernel_size=3,
-                    stride=(2, 1),
-                    padding=1,
-                )
-            ),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(dropout),
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=48,
-                    out_channels=96,
-                    kernel_size=3,
-                    stride=(2, 1),
-                    padding=1,
-                )
-            ),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(dropout),
-        )
-        self.linear = torch.nn.Conv1d(
-            96 * math.ceil(in_channels / 4), out_channels, kernel_size=1,
-        )
-
-    def forward(self, x):
-        x = x.unsqueeze(1)
-        x = self.conv2ds(x)
-        x = x.flatten(1, 2)
-        x = self.linear(x)
-        return x
-
-
-class Acoustic(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        in_channels = constants.SPEC_N_BINS
-        out_channels = constants.MAX_PITCH - constants.MIN_PITCH + 1
-        dropout = 0.2
-        self.onsets_stack = _Conv2dStack(in_channels, out_channels, dropout)
-        self.offsets_stack = _Conv2dStack(in_channels, out_channels, dropout)
-        self.actives_stack = _Conv2dStack(in_channels, out_channels, dropout)
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=1, out_channels=48, kernel_size=3, padding=1
+        )
+        self.activation1 = torch.nn.ReLU()
 
-    def forward(self, spec):
-        pianoroll = {}
-        pianoroll["onsets"] = self.onsets_stack(spec)
-        pianoroll["offsets"] = self.offsets_stack(spec)
-        pianoroll["actives"] = self.actives_stack(spec)
-        return pianoroll
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=48, out_channels=48, kernel_size=3, padding=1
+        )
+        self.activation2 = torch.nn.ReLU()
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=(2, 1))
+        self.dropout2 = torch.nn.Dropout(0.25)
+
+        self.conv3 = torch.nn.Conv2d(
+            in_channels=48, out_channels=96, kernel_size=3, padding=1
+        )
+        self.activation3 = torch.nn.ReLU()
+        self.pool3 = torch.nn.MaxPool2d(kernel_size=(2, 1))
+        self.dropout3 = torch.nn.Dropout(0.25)
+
+        self.linear = torch.nn.Conv1d(
+            in_channels=96 * (constants.SPEC_N_BINS // 4),
+            out_channels=768,
+            kernel_size=1,
+        )
+        self.activation_linear = torch.nn.ReLU()
+        self.dropout_linear = torch.nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, dim=1)
+        x = self.conv1(x)
+        x = self.activation1(x)
+
+        x = self.conv2(x)
+        x = self.activation2(x)
+        x = self.pool2(x)
+        x = self.dropout2(x)
+
+        x = self.conv3(x)
+        x = self.activation3(x)
+        x = self.pool3(x)
+        x = self.dropout3(x)
+        x = torch.flatten(x, start_dim=1, end_dim=2)
+
+        x = self.linear(x)
+        x = self.activation_linear(x)
+        x = self.dropout_linear(x)
+        return x
