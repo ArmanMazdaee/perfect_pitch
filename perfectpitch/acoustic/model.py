064819d4cebf2e96f0ca717b5c4110ce738ce584
Arman Mazdaee <arman.maz1373@gmail.com>
2020-06-10 19:41:27 +0430

    update the AcousticModel

--- perfectpitch/acoustic/model.py
+++ perfectpitch/acoustic/model.py
@@ -1,86 +1,82 @@
 import torch
 import torch.nn as nn
 
-from perfectpitch import constants
 
-
-class _Conv2dStack(nn.Module):
-    def __init__(self):
+class _ResidualBlock(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, dilation,
+    ):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=48, kernel_size=3, padding=1)
-        self.activation1 = nn.ReLU()
-
-        self.conv2 = nn.Conv2d(
-            in_channels=48, out_channels=48, kernel_size=3, padding=1
+        self.conv1 = torch.nn.utils.weight_norm(
+            torch.nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                padding=(1, dilation),
+                dilation=(1, dilation),
+            )
         )
-        self.activation2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 1))
-        self.dropout2 = nn.Dropout(0.25)
-
-        self.conv3 = nn.Conv2d(
-            in_channels=48, out_channels=96, kernel_size=3, padding=1
+        self.dropout1 = torch.nn.Dropout(0.2)
+        self.conv2 = torch.nn.utils.weight_norm(
+            torch.nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                padding=(1, dilation),
+                dilation=(1, dilation),
+            )
         )
-        self.activation3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(kernel_size=(2, 1))
-        self.dropout3 = nn.Dropout(0.25)
+        self.dropout2 = torch.nn.Dropout(0.2)
 
-        self.linear = nn.Conv1d(
-            in_channels=96 * (constants.SPEC_N_BINS // 4),
-            out_channels=768,
-            kernel_size=1,
-        )
-        self.activation_linear = nn.ReLU()
-        self.dropout_linear = nn.Dropout(0.5)
+        self.downsample = None
+        if in_channels != out_channels:
+            self.downsample = torch.nn.utils.weight_norm(
+                torch.nn.Conv2d(
+                    in_channels=in_channels, out_channels=out_channels, kernel_size=1
+                )
+            )
 
-    def forward(self, x):
-        x = torch.unsqueeze(x, dim=1)
-        x = self.conv1(x)
-        x = self.activation1(x)
-
+    def forward(self, spec):
+        x = self.conv1(spec)
+        x = torch.nn.functional.relu(x)
+        x = self.dropout1(x)
         x = self.conv2(x)
-        x = self.activation2(x)
-        x = self.pool2(x)
+        x = torch.nn.functional.relu(x)
         x = self.dropout2(x)
 
-        x = self.conv3(x)
-        x = self.activation3(x)
-        x = self.pool3(x)
-        x = self.dropout3(x)
-        x = torch.flatten(x, start_dim=1, end_dim=2)
+        res = self.downsample(spec) if self.downsample is not None else spec
+        return torch.nn.functional.relu(res + x)
 
-        x = self.linear(x)
-        x = self.activation_linear(x)
-        x = self.dropout_linear(x)
-        return x
+
+class _Conv2dStack(torch.nn.Sequential):
+    def __init__(self, in_channels):
+        super().__init__(
+            _ResidualBlock(in_channels=in_channels, out_channels=16, dilation=1),
+            _ResidualBlock(in_channels=16, out_channels=64, dilation=2),
+            _ResidualBlock(in_channels=64, out_channels=64, dilation=4),
+            _ResidualBlock(in_channels=64, out_channels=16, dilation=16),
+            _ResidualBlock(in_channels=16, out_channels=1, dilation=32),
+        )
 
 
 class AcousticModel(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.onsets_conv2d_stack = _Conv2dStack()
-        self.onsets_linear = torch.nn.Conv1d(
-            in_channels=768, out_channels=88, kernel_size=1
-        )
-
-        self.offsets_conv2d_stack = _Conv2dStack()
-        self.offsets_linear = torch.nn.Conv1d(
-            in_channels=768, out_channels=88, kernel_size=1
-        )
-
-        self.actives_conv2d_stack = _Conv2dStack()
-        self.actives_linear = torch.nn.Conv1d(
-            in_channels=768, out_channels=88, kernel_size=1
-        )
+        self.onsets_conv2d_stack = _Conv2dStack(in_channels=1)
+        self.offsets_conv2d_stack = _Conv2dStack(in_channels=2)
+        self.actives_conv2d_stack = _Conv2dStack(in_channels=2)
 
     def forward(self, spec):
-        onsets = self.onsets_conv2d_stack(spec)
-        onsets = self.onsets_linear(onsets)
+        onsets_input = spec.unsqueeze(dim=1)
+        onsets_output = self.onsets_conv2d_stack(onsets_input)
 
-        offsets = self.offsets_conv2d_stack(spec)
-        offsets = self.offsets_linear(offsets)
+        onsets = torch.sigmoid(onsets_output).detach()
+        offsets_input = torch.cat([onsets_input, onsets], dim=1)
+        offsets_output = self.offsets_conv2d_stack(offsets_input)
+        actives_output = self.actives_conv2d_stack(offsets_input)
 
-        actives = self.actives_conv2d_stack(spec)
-        actives = self.actives_linear(actives)
-
-        return {"onsets": onsets, "offsets": offsets, "actives": actives}
+        return {
+            "onsets": onsets_output.squeeze(1),
+            "offsets": offsets_output.squeeze(1),
+            "actives": actives_output.squeeze(1),
+        }
