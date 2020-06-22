183a9ff247761e94f6c299e830710d0a80863bf3
Arman Mazdaee <arman.maz1373@gmail.com>
2020-06-22 22:50:11 +0430

    change TranscriptionDataset to a IterableDataset

--- perfectpitch/dataset/transcription_dataset.py
+++ perfectpitch/dataset/transcription_dataset.py
@@ -1,27 +1,38 @@
 import os
+import random
 from glob import glob
 
 import torch
 
 
-class TranscriptionDataset(torch.utils.data.Dataset):
-    def __init__(self, path):
-        sample_filenames = sorted(glob(os.path.join(path, "*.pt")))
+class TranscriptionDataset(torch.utils.data.IterableDataset):
+    def __init__(self, path, shuffle):
+        self._path = path
+        self._shuffle = shuffle
+
+    def __iter__(self):
+        sample_filenames = sorted(glob(os.path.join(self._path, "*.pt")))
         if len(sample_filenames) == 0:
-            raise ValueError(f"No sample found in {path}")
+            raise RuntimeError(f"No sample found in {self._path}")
 
-        self.__sample_filenames = sample_filenames
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is not None:
+            sample_filenames = [
+                f
+                for i, f in enumerate(sample_filenames)
+                if i % worker_info.num_workers == worker_info.id
+            ]
 
-    def __len__(self):
-        return len(self.__sample_filenames)
+        if self._shuffle:
+            random.shuffle(sample_filenames)
 
-    def __getitem__(self, index):
-        sample = torch.load(self.__sample_filenames[index])
-        return {
-            "spec": sample["spec"],
-            "notesequence": {
-                "pitches": sample["pitches"],
-                "intervals": sample["intervals"],
-                "velocities": sample["velocities"],
-            },
-        }
+        for sample_filename in sample_filenames:
+            sample = torch.load(sample_filename)
+            yield (
+                sample["spec"],
+                {
+                    "pitches": sample["pitches"],
+                    "intervals": sample["intervals"],
+                    "velocities": sample["velocities"],
+                },
+            )
