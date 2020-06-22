31af5d92a30c5736ffe80e2c579a1fd746fafc4e
Arman Mazdaee <arman.maz1373@gmail.com>
2020-06-22 22:50:34 +0430

    change OnsetsDataset to a IterableDataset

--- perfectpitch/onsetsdetector/dataset.py
+++ perfectpitch/onsetsdetector/dataset.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 
 from perfectpitch.dataset.transcription_dataset import TranscriptionDataset
@@ -5,51 +7,43 @@ from perfectpitch.utils.data import notesequence_to_pianoroll
 
 
 class OnsetsDataset(TranscriptionDataset):
-    def __init__(self, path, min_length=None, max_length=None, pad_sequences=False):
-        super().__init__(path)
+    def __init__(self, path, shuffle, min_length=None, max_length=None, buffer_size=64):
+        super().__init__(path, shuffle)
+        self._min_length = min_length
+        self._max_length = max_length
+        self._buffer_size = buffer_size
 
-        if max_length is None and pad_sequences:
-            raise ValueError("pad_sequences could not be true while max_length is None")
-        self.__sequences_length = max_length if pad_sequences else None
+    def __iter__(self):
+        min_length = 0 if self._min_length is None else self._min_length
+        buffer_size = self._buffer_size if self._shuffle else 1
+        buffer = []
 
-        sample_splits = []
-        for index in range(super().__len__()):
-            sample = super().__getitem__(index)
-            length = sample["spec"].shape[1]
-            step = length if max_length is None else max_length
+        for spec, notesequence in super().__iter__():
+            length = spec.shape[1]
+            pianoroll = notesequence_to_pianoroll(
+                notesequence["pitches"],
+                notesequence["intervals"],
+                notesequence["velocities"],
+                length,
+            )
+            onsets = pianoroll["onsets"]
+            weights = torch.ones_like(onsets)
+            weights[onsets == 1] = 3
+
+            step = length if self._max_length is None else self._max_length
             for start in range(0, length, step):
                 end = min(length, start + step)
-                if min_length is None or end - start >= min_length:
-                    sample_splits.append((index, start, end))
-        self.__sample_splits = sample_splits
+                if end - start < min_length:
+                    break
 
-    def __len__(self):
-        return len(self.__sample_splits)
+                buffer.append(
+                    (spec[:, start:end], onsets[:, start:end], weights[:, start:end])
+                )
 
-    def __getitem__(self, index):
-        sample_index, start, end = self.__sample_splits[index]
-        length = end - start
-        sample = super().__getitem__(sample_index)
+            if self._shuffle:
+                random.shuffle(buffer)
 
-        spec = sample["spec"]
-        sample_length = spec.shape[1]
+            while len(buffer) > buffer_size:
+                yield buffer.pop()
 
-        notesequence = sample["notesequence"]
-        pianoroll = notesequence_to_pianoroll(
-            notesequence["pitches"],
-            notesequence["intervals"],
-            notesequence["velocities"],
-            sample_length,
-        )
-        onsets = pianoroll["onsets"]
-
-        if length < sample_length:
-            spec = spec[:, start:end]
-            onsets = onsets[:, start:end]
-
-        if self.__sequences_length is not None and self.__sequences_length > length:
-            pad = (0, self.__sequences_length - length)
-            spec = torch.nn.functional.pad(spec, pad)
-            onsets = torch.nn.functional.pad(onsets, pad)
-
-        return spec, onsets
+        yield from buffer
