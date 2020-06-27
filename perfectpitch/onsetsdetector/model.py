8012b735bbe5fc248db59284f2e90647d8ac31a1
Arman Mazdaee <arman.maz1373@gmail.com>
2020-06-27 21:11:32 +0430

    make OnsetDetector fatter

--- perfectpitch/onsetsdetector/model.py
+++ perfectpitch/onsetsdetector/model.py
@@ -49,16 +49,16 @@ class OnsetsDetector(torch.nn.Module):
         self.conv2d_stack = torch.nn.Sequential(
             _Conv2dResidualBlock(in_channels=1, out_channels=8, dilation=1),
             _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=2),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=4),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=8),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=16),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=32),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=64),
-            _Conv2dResidualBlock(in_channels=8, out_channels=8, dilation=128),
+            _Conv2dResidualBlock(in_channels=8, out_channels=16, dilation=4),
+            _Conv2dResidualBlock(in_channels=16, out_channels=16, dilation=8),
+            _Conv2dResidualBlock(in_channels=16, out_channels=32, dilation=16),
+            _Conv2dResidualBlock(in_channels=32, out_channels=32, dilation=32),
+            _Conv2dResidualBlock(in_channels=32, out_channels=64, dilation=64),
+            _Conv2dResidualBlock(in_channels=64, out_channels=64, dilation=128),
         )
         self.linear_stack = torch.nn.Sequential(
             torch.nn.Conv1d(
-                in_channels=num_pitches * 8, out_channels=256, kernel_size=1
+                in_channels=num_pitches * 64, out_channels=256, kernel_size=1,
             ),
             torch.nn.ReLU(),
             torch.nn.Conv1d(in_channels=256, out_channels=num_pitches, kernel_size=1),
