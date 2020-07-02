c971de5340dad5e8406eacbefef3d3e287f7d65c
Arman Mazdaee <arman.maz1373@gmail.com>
2020-07-02 22:16:06 +0430

    use pad_sequence in the padded_collate

--- perfectpitch/utils/dataloader.py
+++ perfectpitch/utils/dataloader.py
@@ -1,25 +1,18 @@
 import torch
 
 
-def _pad_to_shape(tensor, shape):
-    pad = []
-    for i in range(len(shape)):
-        pad.append(shape[i] - tensor.shape[i])
-        pad.append(0)
-    pad.reverse()
-    return torch.nn.functional.pad(tensor, pad)
-
-
-def padded_collate(batch):
+def padded_collate(batch, batch_first=False, padding_value=0):
     elem = batch[0]
     if isinstance(elem, torch.Tensor):
-        padded_shape = [max([b.shape[i] for b in batch]) for i in range(elem.ndim)]
-        padded_batch = [_pad_to_shape(t, padded_shape) for t in batch]
-        return torch.stack(padded_batch)
+        return torch.nn.utils.rnn.pad_sequence(batch, batch_first, padding_value)
     elif type(elem) == dict:
-        return {key: padded_collate([b[key] for b in batch]) for key in elem.keys()}
+        return {
+            key: padded_collate([b[key] for b in batch], batch_first, padding_value)
+            for key in elem.keys()
+        }
     elif type(elem) == tuple:
         return tuple(
-            padded_collate([b[index] for b in batch]) for index, _ in enumerate(elem)
+            padded_collate([b[index] for b in batch], batch_first, padding_value)
+            for index, _ in enumerate(elem)
         )
     raise TypeError(f"{type(elem)} is not supported")
