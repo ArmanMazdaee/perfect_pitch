a33ad9635311bf8f0b1b37410c9f679b1b72c0a9
Arman Mazdaee <arman.maz1373@gmail.com>
2020-07-07 17:37:31 +0430

    update setup.py and move back to librosa

--- setup.py
+++ setup.py
@@ -13,10 +13,13 @@ setup(
     entry_points={"console_scripts": ["perfectpitch = perfectpitch:main"]},
     python_requires=">=3.6,<4.0",
     install_requires=[
-        "torch>=1.5.0,<2.0.0",
-        "torchaudio>=0.5.0,<0.6.0",
-        "tqdm>=4.42.0,<5.0.0",
-        "mido>=1.2.0,<2.0.0",
-        "mir_eval>=0.5.0,<0.6.0",
+        "numpy>=1.18.0",
+        "soundfile >= 0.9.0",
+        "numba==0.48.0",  # pin numba version for librosa
+        "librosa>=0.7.2",
+        "mido>=1.2.0",
+        "torch>=1.5.0",
+        "mir_eval>=0.5.0",
+        "tqdm>=4.42.0",
     ],
 )
