088f7d9f038753c22b027416f0def83c3e818bc2
Arman Mazdaee <arman.maz1373@gmail.com>
2020-07-07 19:32:50 +0430

    convert np.ndarray to torch.Tensor in padded_collate

--- perfectpitch/utils/dataloader.py
+++ perfectpitch/utils/dataloader.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 
@@ -5,6 +6,10 @@ def padded_collate(batch, batch_first=False, padding_value=0):
     elem = batch[0]
     if isinstance(elem, torch.Tensor):
         return torch.nn.utils.rnn.pad_sequence(batch, batch_first, padding_value)
+    elif isinstance(elem, np.ndarray):
+        return padded_collate(
+            [torch.from_numpy(t) for t in batch], batch_first, padding_value
+        )
     elif type(elem) == dict:
         return {
             key: padded_collate([b[key] for b in batch], batch_first, padding_value)
