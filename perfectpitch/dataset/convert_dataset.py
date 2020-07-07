8f2778e16cbd925b51a6a300f5996630d2d5cb14
Arman Mazdaee <arman.maz1373@gmail.com>
2020-07-07 21:29:48 +0430

    convert dataset in using multiple process for speed up

--- perfectpitch/dataset/convert_dataset.py
+++ perfectpitch/dataset/convert_dataset.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 
 import numpy as np
 from tqdm import tqdm
@@ -7,20 +8,25 @@ from perfectpitch.utils.audio import load_audio, audio_to_spec
 from perfectpitch.utils.transcription import load_transcription
 
 
+def _convert_sample(args):
+    sample_filenames, wav_filename, midi_filename = args
+    audio = load_audio(wav_filename)
+    spec = audio_to_spec(audio)
+    transcription = load_transcription(midi_filename)
+    np.savez(
+        sample_filenames,
+        spec=spec,
+        pitches=transcription["pitches"],
+        intervals=transcription["intervals"],
+        velocities=transcription["velocities"],
+    )
+
+
 def convert_dataset(output_path, names, wav_filenames, midi_filenames):
     os.makedirs(output_path, exist_ok=True)
-    for name, wav_filename, midi_filename in tqdm(
-        zip(names, wav_filenames, midi_filenames),
-        desc=f"creating {output_path}",
-        total=len(names),
-    ):
-        audio = load_audio(wav_filename)
-        spec = audio_to_spec(audio)
-        transcription = load_transcription(midi_filename)
-        np.savez(
-            os.path.join(output_path, f"{name}.npz"),
-            spec=spec,
-            pitches=transcription["pitches"],
-            intervals=transcription["intervals"],
-            velocities=transcription["velocities"],
-        )
+    sample_filenames = [os.path.join(output_path, f"{name}.npz") for name in names]
+    args = zip(sample_filenames, wav_filenames, midi_filenames)
+    with multiprocessing.Pool() as pool:
+        results = pool.imap_unordered(_convert_sample, args)
+        for _ in tqdm(results, total=len(names), desc=f"creating {output_path}",):
+            pass
