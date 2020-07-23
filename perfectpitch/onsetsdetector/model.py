8390ee5266c856f4d43586735347965f68209d76
Arman Mazdaee <arman.maz1373@gmail.com>
2020-07-23 19:56:58 +0430

    add residual blocks to conv2d

--- perfectpitch/onsetsdetector/model.py
+++ perfectpitch/onsetsdetector/model.py
@@ -3,34 +3,62 @@ import torch
 from perfectpitch import constants
 
 
+DROPOUT = 0.2
+
+
+class _Conv2dResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = torch.nn.utils.weight_norm(
+            torch.nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                padding=1,
+            )
+        )
+        self.dropout1 = torch.nn.Dropout(DROPOUT)
+
+        self.conv2 = torch.nn.utils.weight_norm(
+            torch.nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                padding=1,
+            )
+        )
+        self.dropout2 = torch.nn.Dropout(DROPOUT)
+
+        self.downsample = None
+        if in_channels != out_channels:
+            self.downsample = torch.nn.utils.weight_norm(
+                torch.nn.Conv2d(
+                    in_channels=in_channels, out_channels=out_channels, kernel_size=1
+                )
+            )
+
+    def forward(self, input_):
+        x = self.conv1(input_)
+        x = torch.nn.functional.relu(x)
+        x = self.dropout1(x)
+
+        x = self.conv2(x)
+        x = torch.nn.functional.relu(x)
+        x = self.dropout2(x)
+
+        res = self.downsample(input_) if self.downsample is not None else input_
+        return torch.nn.functional.relu(res + x)
+
+
 class OnsetsDetector(torch.nn.Module):
     def __init__(self):
         super().__init__()
         num_pitches = constants.MAX_PITCH - constants.MIN_PITCH + 1
         self.conv2d = torch.nn.Sequential(
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=1, out_channels=32, kernel_size=3, padding=1
-                )
-            ),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(0.2),
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=32, out_channels=32, kernel_size=3, padding=1
-                )
-            ),
-            torch.nn.ReLU(),
+            _Conv2dResidualBlock(in_channels=1, out_channels=32),
             torch.nn.MaxPool2d(kernel_size=(2, 1)),
-            torch.nn.Dropout(0.2),
-            torch.nn.utils.weight_norm(
-                torch.nn.Conv2d(
-                    in_channels=32, out_channels=64, kernel_size=3, padding=1
-                )
-            ),
-            torch.nn.ReLU(),
+            _Conv2dResidualBlock(in_channels=32, out_channels=64),
             torch.nn.MaxPool2d(kernel_size=(2, 1)),
-            torch.nn.Dropout(0.2),
         )
         self.linear1 = torch.nn.Sequential(
             torch.nn.Linear(
@@ -38,17 +66,17 @@ class OnsetsDetector(torch.nn.Module):
                 out_features=512 - constants.POSENC_DIM,
             ),
             torch.nn.ReLU(),
-            torch.nn.Dropout(0.2),
+            torch.nn.Dropout(p=DROPOUT),
         )
         self.sequential = torch.nn.TransformerEncoder(
             encoder_layer=torch.nn.TransformerEncoderLayer(
                 d_model=512,
                 nhead=4,
                 dim_feedforward=512,
-                dropout=0.2,
+                dropout=DROPOUT,
                 activation="relu",
             ),
-            num_layers=4,
+            num_layers=2,
         )
         self.linear2 = torch.nn.Linear(in_features=512, out_features=num_pitches)
 
