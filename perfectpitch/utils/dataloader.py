2ea9ca47945a664c20d168bef4f4a15c7a5f9fb8
Arman Mazdaee <arman.maz1373@gmail.com>
2020-08-10 10:00:38 +0430

    make batch first in the padded_collate

--- perfectpitch/utils/dataloader.py
+++ perfectpitch/utils/dataloader.py
@@ -2,22 +2,16 @@ import numpy as np
 import torch
 
 
-def padded_collate(batch, batch_first=False, padding_value=0):
+def padded_collate(batch):
     elem = batch[0]
     if isinstance(elem, torch.Tensor):
-        return torch.nn.utils.rnn.pad_sequence(batch, batch_first, padding_value)
+        return torch.nn.utils.rnn.pad_sequence(batch, batch_first=True)
     elif isinstance(elem, np.ndarray):
-        return padded_collate(
-            [torch.from_numpy(t) for t in batch], batch_first, padding_value
-        )
+        return padded_collate([torch.from_numpy(t) for t in batch])
     elif type(elem) == dict:
-        return {
-            key: padded_collate([b[key] for b in batch], batch_first, padding_value)
-            for key in elem.keys()
-        }
+        return {key: padded_collate([b[key] for b in batch]) for key in elem.keys()}
     elif type(elem) == tuple:
         return tuple(
-            padded_collate([b[index] for b in batch], batch_first, padding_value)
-            for index, _ in enumerate(elem)
+            padded_collate([b[index] for b in batch]) for index, _ in enumerate(elem)
         )
     raise TypeError(f"{type(elem)} is not supported")
