217dc105096dd16482eb8b2c898bdd7e2c364f36
Arman Mazdaee <arman.maz1373@gmail.com>
2020-08-14 18:48:40 +0430

    add convert datasets commands

--- perfectpitch/__init__.py
+++ perfectpitch/__init__.py
@@ -1,5 +1,8 @@
 import argparse
 
+import perfectpitch.convert_dataset.convert_maps
+import perfectpitch.convert_dataset.convert_maestro
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -7,6 +10,56 @@ def main():
     )
     subparsers = parser.add_subparsers(title="command")
     subparsers.required = True
+
+    convert_maps_parser = subparsers.add_parser(
+        "convert-maps",
+        help="Convert the MAPS dataset for the perfectpitch",
+        description=(
+            "Convert the MAPS datasets to a format which is useable by perfectpitch"
+        ),
+    )
+    convert_maps_parser.set_defaults(
+        func=perfectpitch.convert_dataset.convert_maps.convert_maps
+    )
+    convert_maps_parser.add_argument(
+        "--input",
+        "-i",
+        required=True,
+        help="Path of the extracted MAPS dataset",
+        dest="input_path",
+    )
+    convert_maps_parser.add_argument(
+        "--output",
+        "-o",
+        required=True,
+        help="Path of the converted dataset",
+        dest="output_path",
+    )
+
+    convert_maestro_parser = subparsers.add_parser(
+        "convert-maestro",
+        help="Convert the MAESTRO dataset for the perfectpitch",
+        description=(
+            "Convert the MAESTRO datasets to a format which is useable by perfectpitch"
+        ),
+    )
+    convert_maestro_parser.set_defaults(
+        func=perfectpitch.convert_dataset.convert_maestro.convert_maestro
+    )
+    convert_maestro_parser.add_argument(
+        "--input",
+        "-i",
+        required=True,
+        help="Path of the extracted MAESTRO dataset",
+        dest="input_path",
+    )
+    convert_maestro_parser.add_argument(
+        "--output",
+        "-o",
+        required=True,
+        help="Path of the converted dataset",
+        dest="output_path",
+    )
 
     args = parser.parse_args()
     func = args.func
